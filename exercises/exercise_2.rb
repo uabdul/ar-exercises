1f3745700d4eadce289e1cbc75ccd3f2bf20d382
Umair Abdul Qadir <umair.abdulq@gmail.com>
2019-01-16 00:02:07 +0000

    Completing second exercise.

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,11 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find_by(id: 1)
+puts "This is the initial store 1 name: #{@store1.name}"
+@store1.update(name: 'Woof Town')
+puts "This is the new store 1 name: #{@store1.name}"
+@store2 = Store.find_by(id: 2)
+puts "This is the initial store 2 name: #{@store2.name}"
+@store2.update(name: 'Dog Palace')
+puts "This is the new store 2 name: #{@store2.name}"
