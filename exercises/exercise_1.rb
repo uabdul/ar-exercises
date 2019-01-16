8695a2c2a0fbcdca2142ebe118ad4698c4afc82b
Umair Abdul Qadir <umair.abdulq@gmail.com>
2019-01-16 00:10:23 +0000

    Completing third and fourth exercise.

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -8,6 +8,6 @@ puts "----------"
 Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
-# Your code goes below here ...
 
 puts "The total number of stores is: #{Store.count}"
+
